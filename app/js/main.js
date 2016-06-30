be6af67e1a84face890347d5df56254482f39996
mattjo <mattias.johansson@digitaslbi.com>
2016-06-30 17:24:26 +0200

    Added mode animation on text and links

--- app/js/main.js
+++ app/js/main.js
@@ -13,61 +13,77 @@ NIBS.main = (function() {
     var _sparkleControl,
         _dur = 0.2,
         _$inner,
+        _$textWrapper,
+        _textWrapperX = 0,
         _sliderK = 1 / 7,
-        _get;
+        $1 = document.querySelector.bind(document),
+        $2 = document.querySelectorAll.bind(document);
 
-    _get = {
-        $1: document.querySelector.bind(document), //Node array, usage: var el = $1('.one-time-class');
-        $2: document.querySelectorAll.bind(document) // Direct reference, usage: var alArr = $2('.my-class');
-    };
-
-    _$inner = _get.$1('.dlbi-sparkle-banner-inner');
+    _$inner = $1('.dlbi-sparkle-banner-inner');
+    _$textWrapper = $1('.dlbi-sparkle-banner-inner .text-wrapper');
 
     function _setLessMode() {
         _animSlider(0.107, function () {
             _animInner(1);
+            _animTextWrap(0, function () {
+                $1('.the-sparkle-control-wrapper .labels').className = 'labels less';
+            });
         });
     }
 
     function _setMediumMode() {
         _animSlider(0.373, function () {
             _animInner(1);
+            _animTextWrap(1, function () {
+                $1('.the-sparkle-control-wrapper .labels').className = 'labels medium';
+            });
         });
     }
 
     function _setWaaayMode() {
         _animSlider(0.625, function () {
             _animInner(1);
+            _animTextWrap(2);
+            $1('.the-sparkle-control-wrapper .labels').className = 'labels waaay';
         });
     }
 
     function _setCustomMode() {
         _animSlider(0.887, function () {
+            _animTextWrap(2);
             _animInner(0.7, function () {
-
 
             });
         });
     }
 
-    function _animInner(n, callback) {
-
-        var from = parseInt(_$inner.style.width) / 100;
+    function _animTextWrap(to, callback) {
 
         callback = callback || function () {};
-        tw.tween(from, n, _dur, function(data) {
+        var from = _textWrapperX;
+
+        tw.tween(from, to, _dur * 1.3, function(data) {
+             _$textWrapper.style.left = (data.val * -100) + '%';
+            _textWrapperX = data.val;
+        }, callback);
+
+    }
+
+    function _animInner(to, callback) {
+
+        callback = callback || function () {};
+        var from = parseInt(_$inner.style.width) / 100;
+        tw.tween(from, to, _dur, function(data) {
             _$inner.style.width = (data.val * 100) + '%';
             _sparkleControl.resize();
         }, callback);
 
-
     }
 
-    function _animSlider(n, callback) {
+    function _animSlider(to, callback) {
 
         callback = callback || function () {};
-        var from = _sparkleControl.getValue(),
-            to = n;
+        var from = _sparkleControl.getValue();
 
         tw.tween(from, to, _dur, function(data) {
             _sparkleControl.setValue(data.val);
@@ -76,6 +92,22 @@ NIBS.main = (function() {
     }
 
     function _run() {
+
+        var $data = $1('.dlbi-sparkle-banner .data'),
+            code = '',
+            data;
+
+        if ($data) {
+            data = $data.innerText.split('|');
+
+            for (var i = 0; i < data.length; i++) {
+                code += '<div class="fancy_text msg' + i + '">' + data[i] + '</div>';
+            }
+
+            $1('.less-container').innerHTML = code;
+            $1('.medium-container').innerHTML = code;
+            $1('.waaay-container').innerHTML = code;
+        }
 
         _sparkleControl = new MOS.Slider({
             selector: '.the-sparkle-control',
@@ -106,6 +138,7 @@ NIBS.main = (function() {
         //NIBS.logMsg.add();
 
         _setLessMode();
+        //_setCustomMode();
 
     }
 
